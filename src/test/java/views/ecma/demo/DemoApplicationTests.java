2c508cf3efc64fff1e7dc8c4363ea6f99aff4ed2
jonibek <akmuratovj@gmail.com>
2022-08-16 00:47:56 +0500

    Initial commit

---
+++ src/test/java/views/ecma/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package views.ecma.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
